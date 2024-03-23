6188e98bc4c62aab30f5b53b721d90cd3bcb8952
shadowfalcon16 <karanhathwar@gmail.com>
2024-03-23 21:34:22 +0530

    Working on Carousel

--- syncd/app/components/carouselBox.tsx
+++ syncd/app/components/carouselBox.tsx
@@ -1,19 +1,37 @@
 import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
+import CarouselSlide from './carouselSlide';
 
 interface CarouselBoxProps {
     collegeName: string;
   }
 
+
+interface EventItem {
+    eventName: string;
+    eventDesc: string;
+    eventImage: string;
+    eventDate: string;
+    eventTime: string;
+    eventLoc: string;
+}
 export default function CarouselBox(props: CarouselBoxProps) {
-    var items = [
+    const items = [
         {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
+            eventName: "Diplomat Wars 4.0",
+            eventDesc: "Diplomat Wars is the PES MUN Society 's event that de-constructs the skill set of diplomacy and provide a glimpse of the enthralling World of Model UNs. Whether you are a seasoned MUNer or just curious, this is your chance to showcase and polish your analytical and soft skills!",
+            eventImage: "/diplomatwars.png",
+            eventDate: "3rd Feb - 4th Feb 2024",
+            eventTime: "8 AM",
+            eventLoc: "MRD Auditorium"
         },
         {
-            name: "Random Name #2",
-            description: "Hello World!"
+            eventName: "Otakuiz",
+            eventDesc: "Anime lovers and Quizzing enthusiasts join us, QQC on 30th January,2024 for an unforgettable experience as we challenge your quick thinking and knowledge on all things anime. Get a chance to win cash prizes upto 3000 rupees!",
+            eventImage: "/animequiz.png",
+            eventDate: "30th Jan 2024",
+            eventTime: "4 PM",
+            eventLoc: "3rd Floor Seminar Hall, BE Block"
         }
     ]
     
@@ -25,14 +43,7 @@ export default function CarouselBox(props: CarouselBoxProps) {
             {
                 items.map( (item, i) => 
                 <div key={i}>
-                    <Paper>
-                        <h2>{item.name}</h2>
-                        <p>{item.description}</p>
-            
-                        <Button className="CheckButton">
-                            Check it out!
-                        </Button>
-                    </Paper>
+                    <CarouselSlide {...item}/>
                 </div> 
             )}
             </Carousel>
--- syncd/app/components/carouselSlide.tsx
+++ syncd/app/components/carouselSlide.tsx
@@ -1,5 +1,18 @@
-"use state"
+"use client"
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays, faClock } from "@fortawesome/free-regular-svg-icons";
+import { faLocationDot, faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import ColorThief from 'colorthief';
+import NextImage from "next/image";
+type EventItem = {
+    eventName: string;
+    eventDesc: string;
+    eventImage: string;
+    eventDate: string;
+    eventTime: string;
+    eventLoc: string;
+};
 
 type RGBColor = [number, number, number];
 type Palette = RGBColor[];
@@ -10,13 +23,14 @@ const getDarkestColor = (palette: Palette): RGBColor => {
     }, palette[0]);
 };
 
-export default function CarouselSlide() {
-    const ColorThief = require('colorthief');
-    const [mainColor, setMainColor] = useState('#f0f0f0');
 
+
+export default function CarouselSlide(item: EventItem) {
+    
+    const [mainColor, setMainColor] = useState('#f0f0f0');  
     useEffect(() => {
         const colorThief = new ColorThief();
-
+        
         const img = new Image();
         img.src = item.eventImage;
 
@@ -29,6 +43,43 @@ export default function CarouselSlide() {
         });
     }, [item.eventImage]);
     return (
-
+        <div className="flex" style={{background: `linear-gradient(to right, rgb(0,0,0,1) -400px, ${mainColor} 400px)`, height: "310px"}}>
+            
+            <div className="flex p-4 pl-12 flex-col" style={{width: "72em"}}>
+                <h1 className="text-white text-3xl font-semibold">{item.eventName}</h1>
+                <p style={{color: "rgb(255, 255, 255, 0.8)"}}>
+                    <FontAwesomeIcon icon={faCalendarDays}/>
+                    &nbsp;{item.eventDate}&nbsp;&nbsp;&nbsp;
+                    <FontAwesomeIcon icon={faClock}/>
+                    &nbsp;{item.eventTime}&nbsp;&nbsp;&nbsp;
+                    <FontAwesomeIcon icon={faLocationDot}/>
+                    &nbsp;{item.eventLoc}
+                </p>
+                <p style={{color: "rgb(255, 255, 255, 0.8)", width: "80%"}}>{item.eventDesc}</p>
+                <button>
+                    <a style={{ textDecoration: "none" }} href='https://www.google.com' target="_blank" rel="noreferrer">
+                        Register Now! <FontAwesomeIcon icon={faArrowUpRightFromSquare} size='xs'/>
+                    </a>
+                </button>
+                <button>
+                    <a style={{ textDecoration: "none" }} href='www.google.com' target="_blank" rel="noreferrer">
+                        Instagram <FontAwesomeIcon icon={faArrowUpRightFromSquare} size='xs'/>
+                    </a>
+                </button>
+            </div>
+            
+            <div className="flex">
+                <div style={{
+                    background: `linear-gradient(to right, ${mainColor}, rgb(0,0,0,0))`, 
+                    width:"120px", height:"310px",
+                    translate: "80px"
+                    }}></div>
+                <NextImage src={item.eventImage} alt="event" height={500} width={625}/>
+                <div style={{background: `linear-gradient(to right, rgb(0,0,0,0), ${mainColor})`, 
+                    width:"120px", height:"310px",
+                    translate: "-80px"
+                }}></div>
+            </div>
+        </div>
     )
 }
